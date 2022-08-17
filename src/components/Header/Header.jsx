1a5a250b51c5024bdc563d129a47eec0507c2064
Pablo Boschini <pablo.boschini23@gmail.com>
2022-08-17 20:41:03 -0300

    Desafio 1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="contenedor-header">
+            
+            <h2>The Bakery</h2>
+            
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+    return (
+        <main>
+            <section>
+                <h3>MAIN</h3>
+            </section>
+        </main>
+    );
+};
+
+
+export default Main;
