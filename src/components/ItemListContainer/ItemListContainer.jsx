ac7a37bd96c94313daf588b68c557247fa6c1f03
Pablo Boschini <pablo.boschini23@gmail.com>
2022-08-31 00:35:22 -0300

    Finalizando catalogo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,33 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import {productos} from "../../mock/productos";
+import React, { useState } from 'react';
 import ItemList from '../itemList/itemList';
 
 
 
 const ItemListContainer = ({}) => {
    
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-    const getProductos = new Promise((res, rej) => {
-        setTimeout(() => {
-            res(productos);
-        }, 2000);
-    })
-
-
-    getProductos
-        .then((data) => {
-        setItems(data);
-        })
-        .catch((error) => {
-        //console.log(error);
-        })
-        .finally(() => {
-        //console.log('Finally');
-        });
-}, []);
-
+    const items = useState([]);
 
 return (
     <div>
--- src/components/itemList/itemList.jsx
+++ src/components/itemList/itemList.jsx
@@ -1,41 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import {productos} from "../../mock/productos";
+import React, { useState } from 'react';
 import Item from '../item/item';
 
-
-
 const ItemList = ({}) => {
-   
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-    const getProductos = new Promise((res, rej) => {
-        setTimeout(() => {
-            res(productos);
-        }, 2000);
-    })
-
-
-    getProductos
-        .then((data) => {
-        setItems(data);
-        })
-        .catch((error) => {
-        //console.log(error);
-        })
-        .finally(() => {
-        //console.log('Finally');
-        });
-}, []);
+    const items = useState([]);
 
 return (
     <div className='itemList'>        
-       <Item items={items} key={items.id}/>
+       <Item items={items}/>
     </div>
 )
 
 };
-
-
 
 export default ItemList;
